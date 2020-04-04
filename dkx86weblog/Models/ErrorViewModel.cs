81eace8deb3cd29b10b5284f5201b9ce464e0f43
Dmitry Kuznetsov <6352927+DK-x86@users.noreply.github.com>
2020-04-04 23:40:42 +0300

    Add project files.

---
+++ dkx86weblog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dkx86weblog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
