40b7fb49914dd7a493083002eb1d0ebf3b187fd0
Dmitry Kuznetsov <6352927+DK-x86@users.noreply.github.com>
2020-05-09 22:50:42 +0300

    migrated to postress db

--- dkx86weblog/Startup.cs
+++ dkx86weblog/Startup.cs
@@ -28,9 +28,12 @@ namespace dkx86weblog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDbContext<ApplicationDbContext>(options =>options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //string connectionString = Configuration.GetConnectionString("DefaultConnection"); //LOCAL DEV
+            string connectionString = Configuration.GetConnectionString("PgsqlConnection"); // Production
+            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
